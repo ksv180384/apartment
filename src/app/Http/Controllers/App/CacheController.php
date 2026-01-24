65e1329df9698c72abb5d06ec8f5e5a3e1c77c79
s.kotelevsky <s.kotelevsky@sfbtech.org>
2026-01-24 13:16:36 +0300

    Правки

--- src/app/Http/Controllers/App/CacheController.php
+++ src/app/Http/Controllers/App/CacheController.php
@@ -3,14 +3,7 @@
 namespace App\Http\Controllers\App;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\App\NameListResource;
-use App\Http\Resources\App\Property\PropertyResource;
-use App\Http\Resources\App\Property\PropertyShowResource;
-use App\Http\Resources\PaginationResource;
-use App\Services\PropertyService;
-use App\Services\PropertyTypeService;
 use Illuminate\Support\Facades\Artisan;
-use Inertia\Inertia;
 
 class CacheController extends Controller
 {
